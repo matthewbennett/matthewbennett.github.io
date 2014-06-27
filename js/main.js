9effda0995cdd87d95c3440872a6273ee4f44be7
Matthew Bennett <mbennett24@student.gsu.edu>
2014-06-27 19:31:29 -0400

    sparkMarket Demo

--- js/main.js
+++ js/main.js
@@ -12,122 +12,138 @@ function fullscreen() {
  
 fullscreen();
 
- $('span.logreg.log').one('click', function () {
+var setBanner = {
+  init:function(){
+    this.bannerSize();
+    this.setFooter();
+  },
 
-                // insert html
-                $('div.registration-part').append('<p>Login</p>');
-            });
-  
+  setFooter:function() {
+    var top = $(document).height();
+    //alert(top);
+    $("#foot").css("top",top - 40);
+  },
 
-  var sendButton = {
-    init: function() {
-      this.send();
-    },
+  bannerSize:function() {
+    var viewportHeight = $(window).height();
+    var stick = viewportHeight -65;
+    $(".banner").css("height",viewportHeight);
+    $(".navList").css("top",stick);
+  }
+};
 
-    send: function() {
-      $(".talkButton").on('mouseenter', function(){
-        $(this).addClass("sent");
-        $('.talkButton p').empty();
-        $(this).prepend('<i class="fa fa-paper-plane fa-2x"></i>');
-       });
-
-      $(".talkButton").on('mouseleave',function(){
-        $(".talkButton").removeClass("sent");
-        $('.talkButton i').remove();
-        $('.talkButton').prepend('<p>Talk About It</p>');
-      });
-    }
-  };
-
-  var stickyHeader = {
+var stickyHeader = {
     init:function() {
       this.stick();
     },
 
     stick: function() {
       $(window).scroll(function(){
+      var viewportHeight = $(window).height();
+      var stick = viewportHeight -65;
       var top =  $(this).scrollTop();
-      var height = $(this).height() - 70;
+      var height = $(this).height() - 60;
       height = (height / 100) * 100;
       if(top > height) {
-        $(".navWrapper").addClass("headerStick");
+        $(".navList").addClass("headerStick");
+        $(".navList").css("top",0);
+        $(".bannerOnly").addClass("blackOut");
       } else {
-        $(".navWrapper").removeClass("headerStick");
+        $(".navList").removeClass("headerStick");
+        $(".bannerOnly").removeClass("blackOut");
+        $(".navList").css("top",stick);
       }
       });
     }
   };
 
-  var likedButton = {
+
+
+  var showInfo = {
     init:function() {
-      this.liked();
+      this.info();
     },
 
-    liked: function() {
-
-    $(".likeButton").on('mouseenter', function(){
-        $(this).animate({height: 150, width: 150 }, 350 ); 
-        $(".likeButton").append("<span>Let me know you liked this project</span>");
+    info:function() {
+      $(".infoLi").mouseenter(function(){
+        var block = $(this).attr("data-id");
+        if(block == 0) { $(".aboutInfo0").show();}
+        if(block == 1) { $(".aboutInfo1").show();}
+        if(block == 2) { $(".aboutInfo2").show();}
       });
-
-        $(".likeButton").on("click", function(){
-        $(this).addClass("liked");
-        $(this).animate({height: 75, width: 75}, 350);
-        $(".likeButton span").remove();
+      $(".infoLi").mouseleave(function(){
+        $(".aboutInfo0").fadeOut("fast");
+        $(".aboutInfo1").fadeOut("fast");
+        $(".aboutInfo2").fadeOut("fast");
       });
-
-      $(".likeButton").on('mouseleave',function(){
-        $(this).animate({height: 75, width: 75}, 350 );
-        $(".likeButton span").remove();
-      });
-
     }
   };
 
-  var skills = {
+  var tabs = {
     init:function() {
-      this.showSkills();
+      this.switchTabs();
     },
 
-    showSkills: function() {
-      $("#skill1").hide();
-      $("#skill2").hide();
-      $("#skill3").hide();
-      $(window).on("scroll", function(){
-        if($(window).scrollTop() > 450){
-          $("#skill1").slideDown(1000);
-        }
-        if($(window).scrollTop() > 550){
-          $("#skill2").slideDown(1000);
-        }
-        if($(window).scrollTop() > 650){
-          $("#skill3").slideDown(1000);
-        }
+    switchTabs:function() {
+      var activeTab = $("#overviewTab");
+      $("#overviewTab").addClass("activeTab");
+      $(".campaignProfile").hide();
+      $(".campaignComments").hide();
+      $(".campaignUpdates").hide();
 
+      $("#profileTab").click(function(){
+        $(activeTab).removeClass("activeTab");
+        activeTab = $("#profileTab");
+        $("#profileTab").addClass("activeTab");
+        $(".campaignSummary").hide();
+        $(".campaignComments").hide();
+        $(".campaignUpdates").hide();
+        $(".campaignBanner").hide();
+        $(".campaignProfile").fadeIn("fast");
+      });
+
+      $("#overviewTab").click(function(){
+        $(activeTab).removeClass("activeTab");
+        activeTab = $("#overviewTab");
+        $("#overviewTab").addClass("activeTab");
+        $(".campaignBanner").fadeIn("fast");
+        $(".campaignSummary").fadeIn("fast");
+        $(".campaignComments").hide();
+        $(".campaignUpdates").hide();
+        $(".campaignProfile").hide();
+      });
+
+      $("#commentsTab").click(function(){
+        $(activeTab).removeClass("activeTab");
+        activeTab = $("#commentsTab");
+        $("#commentsTab").addClass("activeTab");
+        $(".campaignBanner").hide();
+        $(".campaignSummary").hide();
+        $(".campaignComments").fadeIn("fast");
+        $(".campaignUpdates").hide();
+        $(".campaignProfile").hide();
+      });
+
+      $("#updatesTab").click(function(){
+        $(activeTab).removeClass("activeTab");
+        activeTab = $("#updatesTab");
+        $("#updatesTab").addClass("activeTab");
+        $(".campaignBanner").hide();
+        $(".campaignSummary").hide();
+        $(".campaignComments").hide();
+        $(".campaignUpdates").fadeIn("fast");
+        $(".campaignProfile").hide();
       });
     }
-
   };
 
-  var mailME = {
-    init:function(){
-      this.mail();
-    },
-
-    mail:function(){
-      var subject = $("#name").val();
-      var message = $("#message").val();
-      $(".sender").attr('href', 'mailto:mbennett24@student.gsu.edu?subject='+subject+'&body='+message);
-    }
-  };
  
 
   (function() {
-    mailME.init();
-    skills.init();
-    likedButton.init();
-    sendButton.init();
+    setBanner.init();
     stickyHeader.init();
+    showInfo.init();
+    tabs.init();
 
   }()); 
 
